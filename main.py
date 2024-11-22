fb03fe72339be59ac45ca79ae8ff2154a9eea851
HanRix1 <HanRix1@mail.ru>
2024-11-22 19:51:44 +0700

    добваил реализацию cli

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from library.cli import create_parser
 from library.library import add_book, all_book_list, change_status, remove_book, search_book
+import json
 
 def main():
     parser = create_parser()
     args = parser.parse_args()
+    filename = "books.json"
 
     try:
+        with open(filename, "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+            data = []
+    
+    try:
         if args.command == "add":
-            result = add_book(args.title, args.author, args.year)
-            print(result)
+            result = add_book(data, args.title, args.author, args.year)
+            print(f"Книга с ID:{result} добавлена!")
 
         elif args.command == "remove":
-            result = remove_book(args.id)
-            print(result)
+            res = remove_book(data, args.id)
+            print(res)
 
         elif args.command == "search":
-            result = search_book(args.query, args.by)
+            result = search_book(data, args.query, args.by)
             print(result)
 
         elif args.command == "list":
@@ -32,7 +40,8 @@ def main():
     except Exception as e:
         print(f"Ошибка: {e}")
 
+
+
 if __name__ == "__main__":
     main()
 
- 
